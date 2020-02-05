15cc5e42fda5c5a153303ff8c8fe11df6ac8d0a2
Dennis Kats <DKats@carbonite.com>
2020-02-05 17:18:18 -0500

    Added exporting, removing, and updating templates

--- src/extension.ts
+++ src/extension.ts
@@ -48,9 +48,12 @@ export async function activate(context: vscode.ExtensionContext) {
 				return;
 			}
 
+			// Extension configuration
+			const configs = vscode.workspace.getConfiguration("templatify");
+
 			// the default location for templates is within the extension project directory.
 			const defaultTemplateLocation = path.resolve(__dirname, "../templates");
-			const templateLocation = null || defaultTemplateLocation; // TODO: Retireve template location from user/workbench settings/configuration.
+			const templateLocation = configs.get("templateLocation") as string || defaultTemplateLocation;
 
 			// retrieves contents of template directory
 			fs.readdir(templateLocation, { withFileTypes: true }, async (err, files) => {
@@ -101,12 +104,299 @@ export async function activate(context: vscode.ExtensionContext) {
 				}
 			});
 		}),
-		vscode.commands.registerCommand("extension.exportTemplate", () => {
-			// TODO: Implement
+		vscode.commands.registerCommand("extension.exportTemplate", async () => {
+
+			// Display a message box to the user
+			if (!vscode.workspace.workspaceFolders) {
+				vscode.window.showInformationMessage("You must be working from inside a folder to export as template.");
+				return;
+			}
+
+			// Extension configuration
+			const configs = vscode.workspace.getConfiguration("templatify");
+
+			// the default location for templates is within the extension project directory.
+			const defaultTemplateLocation = path.resolve(__dirname, "../templates");
+			const templateLocation = configs.get("templateLocation") as string || defaultTemplateLocation;
+
+			let templateFiles: string[] = fs.readdirSync(templateLocation);
+			let projectName = vscode.workspace.workspaceFolders[0].name;
+
+			// Get unique template name
+			let successfulName = false;
+			let newTemplateName: string | undefined = undefined;
+			while (!successfulName) {
+				newTemplateName = await vscode.window.showInputBox({ prompt: "Give this new template a unique name", value: projectName, placeHolder: "Unique template name" }) as string;
+				let encodedName = encodeName(newTemplateName);
+				if (templateFiles.every(file => file !== encodedName)) {
+					successfulName = true;
+				} else {
+					vscode.window.showInformationMessage("There already exists a template with a similar name. Please name your template differently.");
+				}
+			}
+			if (newTemplateName === undefined) { return; }
+
+			// Terminate command if no template name is specified
+			if (!newTemplateName.trim()) {
+				vscode.window.showInformationMessage("Must specify non-empty template name.");
+				return;
+			}
+
+			// Allow user to specify template description and/or author
+			let newTemplateDescription = await vscode.window.showInputBox({ prompt: "Give this new template a description", placeHolder: "Template description" });
+			if (newTemplateDescription === undefined) { return; }
+			newTemplateDescription = newTemplateDescription.trim();
+
+			let newTemplateAuthor = await vscode.window.showInputBox({ prompt: "Give this template its author(s)", placeHolder: "Template author(s)" });
+			if (newTemplateAuthor === undefined) { return; }
+			newTemplateAuthor = newTemplateAuthor.trim();
+
+			let templateJSON: Template = { templateName: newTemplateName, source: [] };
+			newTemplateAuthor && (templateJSON.author = newTemplateAuthor);
+			newTemplateDescription && (templateJSON.description = newTemplateDescription);
+
+			fs.readdir(vscode.workspace.rootPath as string, { withFileTypes: true }, async (err, files) => {
+				if (err) {
+					vscode.window.showErrorMessage("There was an unexpected error. Try restarting Visual Studio Code.");
+					return;
+				} else {
+					try {
+						await addSourceToTemplate(files, templateJSON);
+						fs.writeFileSync(path.resolve(templateLocation, encodeName(newTemplateName) as string), JSON.stringify(templateJSON));
+						vscode.window.showInformationMessage("Successfully exported project as template!");
+					} catch {
+						vscode.window.showErrorMessage("There was an unexpected error. Try restarting Visual Studio Code.");
+						return;
+					}
+				}
+			});
+
+		}),
+		vscode.commands.registerCommand("extension.removeTemplate", () => {
+
+			// Extension configuration
+			const configs = vscode.workspace.getConfiguration("templatify");
+
+			// the default location for templates is within the extension project directory.
+			const defaultTemplateLocation = path.resolve(__dirname, "../templates");
+			const templateLocation = configs.get("templateLocation") as string || defaultTemplateLocation;
+
+			fs.readdir(templateLocation, { withFileTypes: true }, async (err, files) => {
+				if (err) {
+					vscode.window.showErrorMessage(
+						"Unable to locate template directory. Make sure your template path is pointing to the correct directory."
+					);
+				} else {
+					try {
+						let templates = await getJSONTemplates(templateLocation, files);
+
+						if (!templates.length) {
+							vscode.window.showInformationMessage("Templatify did not find any templates in the templates directory.");
+							return;
+						}
+
+						let templateNames = templates.map(
+							(template, index) => `${index + 1}. ${template.templateName}`
+						);
+
+						let templateChoiceName = await vscode.window.showQuickPick(
+							templateNames,
+							{
+								placeHolder: "Choose a template to remove",
+							}
+						);
+
+						let templateChoiceIndex;
+						if (templateChoiceName) {
+							templateChoiceIndex = parseInt(templateChoiceName.slice(0, templateChoiceName.indexOf("."))) - 1;
+						} else {
+							return;
+						}
+
+						// imports selected template into current project
+						if (templateChoiceIndex >= 0) {
+							fs.unlinkSync(path.resolve(templateLocation, encodeName(templates[templateChoiceIndex].templateName) as string));
+							vscode.window.showInformationMessage("Successfully removed template!");
+						}
+					} catch {
+						vscode.window.showErrorMessage("There was an unexpected error. Try restarting Visual Studio Code.");
+						return;
+					}
+				}
+			});
+
+		}),
+		vscode.commands.registerCommand("extension.updateTemplate", () => {
+
+			// Display a message box to the user
+			if (!vscode.workspace.workspaceFolders) {
+				vscode.window.showInformationMessage("You must be working from inside a folder to import a template.");
+				return;
+			}
+
+			// Extension configuration
+			const configs = vscode.workspace.getConfiguration("templatify");
+
+			// the default location for templates is within the extension project directory.
+			const defaultTemplateLocation = path.resolve(__dirname, "../templates");
+			const templateLocation = configs.get("templateLocation") as string || defaultTemplateLocation;
+
+			// retrieves contents of template directory
+			fs.readdir(templateLocation, { withFileTypes: true }, async (err, files) => {
+				if (err) {
+					vscode.window.showErrorMessage(
+						"Unable to locate template directory. Make sure your template path is pointing to the correct directory."
+					);
+				} else {
+					try {
+						let templates = await getJSONTemplates(templateLocation, files);
+
+						if (!templates.length) {
+							vscode.window.showInformationMessage("Templatify did not find any templates in the templates directory.");
+							return;
+						}
+
+						let templateNames = templates.map(
+							(template, index) => `${index + 1}. ${template.templateName}`
+						);
+
+						let templateChoiceName = await vscode.window.showQuickPick(
+							templateNames,
+							{
+								placeHolder: "Select a template to update",
+							}
+						);
+
+						let templateChoiceIndex: number;
+						if (templateChoiceName) {
+							templateChoiceIndex = parseInt(templateChoiceName.slice(0, templateChoiceName.indexOf("."))) - 1;
+						} else {
+							return;
+						}
+
+						// imports selected template into current project
+						if (templateChoiceIndex >= 0) {
+							let templateJSON = templates[templateChoiceIndex];
+
+							fs.readdir(vscode.workspace.rootPath as string, { withFileTypes: true }, async (err, files) => {
+								if (err) {
+									vscode.window.showErrorMessage("There was an unexpected error. Try restarting Visual Studio Code.");
+									return;
+								} else {
+									try {
+										templateJSON.source = [];
+										await addSourceToTemplate(files, templateJSON);
+										fs.writeFileSync(path.resolve(templateLocation, encodeName(templateJSON.templateName) as string), JSON.stringify(templateJSON));
+										vscode.window.showInformationMessage(`Updated template: ${templates[templateChoiceIndex].templateName}`);
+									} catch {
+										vscode.window.showErrorMessage("There was an unexpected error. Try restarting Visual Studio Code.");
+										return;
+									}
+								}
+							});
+
+						} else {
+							throw Error();
+						}
+					}
+					catch (error) {
+						vscode.window.showErrorMessage("There was an unexpected error. Try restarting Visual Studio Code.");
+						return;
+					}
+				}
+			});
 		})
 	);
 
 	context.subscriptions.push(...disposable);
+}
+
+const codingKey = 13;
+/**
+ * Encodes a string using a caeser cipher. Used to generate 
+ * @param str The string to encode
+ */
+function encodeName(str: string | undefined): string | undefined {
+	if (str) {
+		return `${str.toLowerCase().replace(/[a-z]/g, c => String.fromCharCode((c.charCodeAt(0) - 97 + codingKey) % 26 + 97)).replace(/\s+/g, "")}.template.json`;
+	}
+	return undefined;
+}
+
+/**
+ * Parses files and adds contents to template source.
+ * @param files Array of fs.Dirents of templated project
+ * @param template A reference to the final template JSON that will be outputed
+ */
+async function addSourceToTemplate(files: fs.Dirent[], template: Template): Promise<void[]> {
+	return addContentsToSource(files, vscode.workspace.rootPath as string, template.source);
+}
+
+/**
+ * Add a folder's contents to the given source.
+ * @param files The content files to add to the given source
+ * @param dirPath The path to the given files
+ * @param source The source to save the content files to
+ */
+async function addContentsToSource(files: fs.Dirent[], dirPath: string, source: (File | Directory)[]) {
+	let promises: Promise<void>[] = [];
+	files.forEach(file => {
+		if (file.isFile()) {
+			promises.push(addFileToSource(file, dirPath, source));
+		} else if (file.isDirectory()) {
+			promises.push(addDirectoryToSource(file, dirPath, source));
+		}
+	});
+	return Promise.all(promises);
+}
+
+/**
+ * Adds a specified file to the given source of a template.
+ * @param file The file from the specified directory
+ * @param dirPath The directory to look for the file in
+ * @param source The source to add the file to
+ */
+async function addFileToSource(file: fs.Dirent, dirPath: string, source: (File | Directory)[]): Promise<void> {
+	return new Promise((resolve, reject) => {
+		fs.readFile(path.resolve(dirPath, file.name), (err, data) => {
+			if (err) {
+				reject();
+				return;
+			} else {
+				source.push({
+					filename: file.name,
+					content: escape(data.toString())
+				});
+				resolve();
+			}
+		});
+	});
+}
+
+/**
+ * Adds a specified directory to the given source of a template.
+ * @param dir The inner directory from the specified directory
+ * @param dirPath The directory to look for the inner directory in
+ * @param source The source to add the directory to
+ */
+async function addDirectoryToSource(dir: fs.Dirent, dirPath: string, source: (File | Directory)[]): Promise<void> {
+	return new Promise((resolve, reject) => {
+		let newDirectory = {
+			directoryName: dir.name,
+			source: []
+		};
+		let { source: innerSource } = newDirectory;
+		fs.readdir(path.resolve(dirPath, dir.name), { withFileTypes: true }, async (err, files) => {
+			if (err) {
+				reject();
+				return;
+			} else {
+				await addContentsToSource(files, path.resolve(dirPath, dir.name), innerSource);
+				source.push(newDirectory);
+				resolve();
+			}
+		});
+	});
 }
 
 /**
@@ -229,7 +519,7 @@ async function createFile(currentDir: string, itemFile: File) {
 					newFile = `${newFileRoot} (${++count})${extension}`;
 					resolve();
 				} else {
-					fs.writeFile(newFile, itemFile.content, () => { });
+					fs.writeFile(newFile, unescape(itemFile.content), () => { });
 					successful = true;
 					resolve();
 				}
